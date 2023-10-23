aca39349e7dedd3cd36a9f534825ad4c453b8649
Kabu-Prog <fireeye@hotmail.de>
2023-10-23 15:35:07 +0200

    - Added swap nodes functionality - TODO MergeSort implementation

--- Datastructures.cpp
+++ Datastructures.cpp
@@ -3,54 +3,36 @@
 
 int main()
 {
-	LinkedList<int> ll;
+	LinkedList<std::string> ll;
 
-	ll.Add(0);
-	ll.Add(1);
-	ll.Add(2);
-	ll.Add(3);
-	ll.Add(4);
-	ll.Add(5);
-	ll.Add(6);
+	ll.Add("3");
+	ll.Add("A");
+	ll.Add("B");
+	ll.Add("4");
+	ll.Add("5");
+	ll.Add("6");
 
-	ll.AddAt(7, 4);
-
-	ll.AddAt(8, 0);
-
-	ll.AddAt(9, ll.getLength());
-
-	ll.ReplaceAt(200, 0);
-	ll.ReplaceAt(100, ll.getLength());
-	ll.ReplaceAt(300, 8);
-	ll.ReplaceAt(400, 2);
+	ll.SwapPosition(1, 0);
 
 	ll.Print();
 
-	ll.deleteAt(0);
-	ll.Print();
-
-	ll.deleteAt(ll.getLength());
-
+	ll.invertList();
 
 	ll.Print();
 
-	ll.deleteAt(2);
-	ll.deleteAt(4);
+	ll.SwapValue("A", "B");
 
 	ll.Print();
 
-	ll.Add(3);
-	ll.Add(3);
-	ll.Add(3);
-	ll.Add(3);
-	ll.Add(3);
-	ll.Print();
+	ll.invertList();
 
-	ll.deleteValue(3);
+	ll.AddAt("ABC", 5);
+
+	ll.deleteAmount(3, false);
 
 	ll.Print();
 
-
+	std::cout << ll;
 
 
 }
--- LinkedList.h
+++ LinkedList.h
@@ -2,6 +2,9 @@
 #define LINKEDLIST_H
 
 
+
+//ToDO: Andere Struktur. Statt 15 x delete, etc. zu haben wäre es besser so etwas wie "search by value" oder "search by position" und anschließend die Tätigkeit "delete", "swap", "replace", "edit", ...
+
 template <typename T>
 class LinkedList
 {
@@ -27,14 +30,8 @@ private:
 		}
 		Node* getPrev()
 		{
-			if (mPrev != nullptr)
-			{
-				return mPrev;
-			}
-			else
-			{
-				return nullptr;
-			}
+			return mPrev;
+
 		}
 
 		void setValue(T value)
@@ -61,10 +58,36 @@ private:
 		}
 	};
 
-
+	
 	Node<T>* mFirstNode = nullptr;
 	Node<T>* mLastNode = nullptr;
 	uint32_t mIndex = 0;
+
+	Node<T>* getFirstNode()
+	{
+		return mFirstNode;
+	}
+	Node<T>* getLastNode()
+	{
+		return mLastNode;
+	}
+	uint32_t getIndex()
+	{
+		return mIndex;
+	}
+	void setIndex(int index)
+	{
+		mIndex = index;
+	}
+	void setFirstNode(Node<T>* firstNode)
+	{
+		mFirstNode = firstNode;
+	}
+	void setLastNode(Node<T>* lastNode)
+	{
+		mLastNode = lastNode;
+	}
+
 
 	friend std::ostream& operator<<(std::ostream& os, const LinkedList<T>& ll)
 	{
@@ -85,69 +108,351 @@ private:
 		return os;
 	}
 
-	void deleteNode(Node<T>* node)
+	Node<T>* findNodeByValue(T& value)
 	{
-		bool isError = false;
-		if (node->getPrev() != nullptr && node->getNext() != nullptr)
+		Node<T>* ptrNode = getFirstNode();
+		while (ptrNode != nullptr)
 		{
-			node->getPrev()->setNext(node->getNext());
-			node->getNext()->setPrev(node->getPrev());
-			node->setNext(nullptr);
-			node->setPrev(nullptr);
+			if (ptrNode->getValue() == value)
+			{
+				return ptrNode;
+			}
+			ptrNode = ptrNode->getNext();
 		}
-		else if (node->getPrev() == nullptr)
+		return nullptr;
+	}
+
+	Node<T>* findNodeByPosition(uint32_t position)
+	{
+		uint32_t index = getIndex();
+		if (position >= 0 && position < index)
 		{
-			mFirstNode = node->getNext();
-			node->getNext()->setPrev(nullptr);
-			node->setNext(nullptr);
+			if (position > index / 2)
+			{
+				Node<T>* ptrNode = getLastNode();
+				for (uint32_t i = index-1; i > position; i--)
+				{
+					ptrNode = ptrNode->getPrev();
+				}
+				return ptrNode;
+			}
+			else
+			{
+				Node<T>* ptrNode = getFirstNode();
+
+				for (uint32_t i = 0; i < position; i++)
+				{
+					ptrNode = ptrNode->getNext();
+				}
+				return ptrNode;
+			}
 		}
-		else if (node->getNext() == nullptr)
+		return nullptr;
+	}
+
+	void swapNodes(Node<T>* nodeA, Node<T>* nodeB)
+	{
+		
+		// Spezialfälle: 
+		// Erste, Letzte Node;
+		// Nebeneinander
+
+		if (nodeA != nullptr && nodeB != nullptr)
 		{
-			mLastNode = node->getPrev();
-			node->getPrev()->setNext(nullptr);
-			node->setPrev(nullptr);
+			if (nodeA->getNext() != nodeB && nodeA->getPrev() != nodeB)
+			{
+				Node<T>* nodeAPrev = nodeA->getPrev();
+				Node<T>* nodeANext = nodeA->getNext();
+				Node<T>* nodeBPrev = nodeB->getPrev();
+				Node<T>* nodeBNext = nodeB->getNext();
+
+				nodeA->setPrev(nodeBPrev);
+				nodeA->setNext(nodeBNext);
+				nodeB->setPrev(nodeAPrev);
+				nodeB->setNext(nodeANext);
+
+				if (nodeAPrev != nullptr)
+				{
+					nodeAPrev->setNext(nodeB);
+				}
+				else
+				{
+					setFirstNode(nodeB);
+				}
+				if (nodeANext != nullptr)
+				{
+					nodeANext->setPrev(nodeB);
+				}
+				else
+				{
+					setLastNode(nodeB);
+				}
+				if (nodeBPrev != nullptr)
+				{
+					nodeBPrev->setNext(nodeA);
+				}
+				else
+				{
+					setFirstNode(nodeA);
+				}
+				if (nodeBNext != nullptr)
+				{
+					nodeBNext->setPrev(nodeA);
+				}
+				else
+				{
+					setLastNode(nodeA);
+				}
+
+			}
+			else if (nodeA->getNext() == nodeB)
+			{
+				Node<T>* nodeAPrev = nodeA->getPrev();
+				Node<T>* nodeBNext = nodeB->getNext();
+
+				nodeA->setPrev(nodeB);
+				nodeB->setNext(nodeA);
+
+				nodeA->setNext(nodeBNext);
+				nodeB->setPrev(nodeAPrev);
+
+				if (nodeAPrev != nullptr)
+				{
+					nodeAPrev->setNext(nodeB);
+				}
+				else
+				{
+					setFirstNode(nodeB);
+				}
+				if (nodeBNext != nullptr)
+				{
+					nodeBNext->setPrev(nodeA);
+				}
+				else
+				{
+					setLastNode(nodeA);
+				}
+				
+			}
+			else
+			{
+				Node<T>* nodeANext = nodeA->getNext();
+				Node<T>* nodeBPrev = nodeB->getPrev();
+					
+				nodeA->setNext(nodeB);
+				nodeB->setPrev(nodeA);
+
+				nodeA->setPrev(nodeBPrev);
+				nodeB->setNext(nodeANext);
+
+				if (nodeANext != nullptr)
+				{
+					nodeANext->setPrev(nodeB);
+				}
+				else
+				{
+					setLastNode(nodeB);
+				}
+				if (nodeBPrev != nullptr)
+				{
+					nodeBPrev->setNext(nodeA);
+				}
+				else
+				{
+					setFirstNode(nodeA);
+				}
+			}
 		}
 		else
 		{
-			std::cerr << "Error: Could not delete Node.";
-			isError = true;
+			std::cerr << "Error: Node is a NULL.\n";
 		}
-		if (!isError)
-		{
-			mIndex--;
-		}
+		
+		
 	}
 
+	void deleteNode(Node<T>* node)
+	{
+		bool isError = false;
+		if (node != nullptr)
+		{
+			if (node->getPrev() != nullptr && node->getNext() != nullptr)
+			{
+				node->getPrev()->setNext(node->getNext());
+				node->getNext()->setPrev(node->getPrev());
+				node->setNext(nullptr);
+				node->setPrev(nullptr);
+			}
+			else if (node->getPrev() == nullptr)
+			{
+				mFirstNode = node->getNext();
+				node->getNext()->setPrev(nullptr);
+				node->setNext(nullptr);
+			}
+			else if (node->getNext() == nullptr)
+			{
+				mLastNode = node->getPrev();
+				node->getPrev()->setNext(nullptr);
+				node->setPrev(nullptr);
+			}
+			else
+			{
+				std::cerr << "Error: Could not delete Node.\n";
+				isError = true;
+			}
+			if (!isError)
+			{
+				mIndex--;
+			}
+		}
+		else
+		{
+			std::cerr << "Error: Could not delete Node. The list seems to be empty.\n";
+		}
+
+	}
+
+
+	//TODO: MergeSort
+	Node<T>* MergeSort(Node<T>* firstNode)
+	{
+		bool print = true;
+
+		Node<T>* firstHalf = firstNode;
+		Node<T>* secondHalf = firstNode;
+
+		while(secondHalf != nullptr && secondHalf->getNext() != nullptr)
+		{
+			secondHalf = secondHalf->getNext();
+			if (secondHalf->getNext() != nullptr)
+			{
+				firstHalf = firstHalf->getNext();
+				secondHalf = secondHalf->getNext();
+			}
+		}
+
+		secondHalf = firstHalf->getNext();
+		firstHalf = firstNode;
+		secondHalf->getPrev()->setNext(nullptr);
+		secondHalf->setPrev(nullptr);
+
+		if (print)
+		{
+			Node<T>* ptrNode;
+			std::cout << "first: ";
+			ptrNode = firstHalf;
+			while (ptrNode != nullptr)
+			{
+				std::cout << ptrNode->getValue() << " ";
+				ptrNode = ptrNode->getNext();
+			}
+			std::cout << "\n";
+			std::cout << "second: ";
+			ptrNode = secondHalf;
+			while (ptrNode != nullptr)
+			{
+				std::cout << ptrNode->getValue() << " ";
+				ptrNode = ptrNode->getNext();
+			}
+			std::cout << "\n";
+		}
+
+		Node<T>* a = nullptr;
+		Node<T>* b = nullptr;
+		Node<T>* newFirstNode = nullptr;
+
+		if (firstHalf->getNext() != nullptr)
+		{
+			a = MergeSort(firstHalf);
+		}
+		if (secondHalf->getNext() != nullptr)
+		{
+			b = MergeSort(secondHalf);
+		}
+
+		Node<T>* merged = new Node<T>;
+		Node<T>* temp = new Node<T>;
+
+		merged = temp;
+
+		while (a != nullptr && b != nullptr)
+		{
+			if (a->getValue() <= b->getValue())
+			{
+				temp->getNext()->setNext(a);
+				a = a->getNext();
+			}
+			else
+			{
+				temp->getNext()->setNext(b);
+				b = b->getNext();
+			}
+			temp = temp->getNext();
+			std::cout << temp->getValue();
+		}
+
+		if (print)
+		{
+			Node<T>* ptrNode;
+			std::cout << "merged: ";
+			ptrNode = temp;
+			while (ptrNode != nullptr)
+			{
+				std::cout << ptrNode->getValue() << " ";
+				ptrNode = ptrNode->getNext();
+			}
+			std::cout << "\n";
+		}
+		
+		return temp;
+	}
+
+
 public:
+	void MergeSort(LinkedList<T>& ll)
+	{
+		MergeSort(ll.getFirstNode());
+	}
+
+	void SwapPosition(uint32_t positionA, uint32_t positionB)
+	{
+		swapNodes(findNodeByPosition(positionA), findNodeByPosition(positionB));
+	}
+	void SwapValue(T ValueA, T ValueB)
+	{
+		swapNodes(findNodeByValue(ValueA), findNodeByValue(ValueB));
+	}
+
 	// Adds a node at the end of the linked list
 	void Add(T value)
 	{
-		if (mFirstNode == nullptr)
+		if (getFirstNode() == nullptr)
 		{
 			Node<T>* newNode = new Node<T>(value, nullptr, nullptr);
-			mFirstNode = newNode;
-			mLastNode = newNode;
+			setFirstNode(newNode);
+			setLastNode(newNode);
 		}
 		else
 		{
 			Node<T>* newNode = new Node<T>(value, nullptr, mLastNode);
-			mLastNode->setNext(newNode);
-			mLastNode = newNode;
+			getLastNode()->setNext(newNode);
+			setLastNode(newNode);
 		}
-		mIndex++;
+		setIndex(getIndex() + 1);
 	}
 
 	// Adds a node at the given position
 	void AddAt(T value, uint32_t position)
 	{
 		bool isError = false;
-		Node<T>* ptrNode;
-		if (position > 0 && position < mIndex-1)
+		uint32_t index = getIndex();
+		Node<T>* ptrNode = nullptr;
+		if (position > 0 && position < index - 1)
 		{
-			if (position > mIndex / 2)
+			if (position > index / 2)
 			{
-				ptrNode = mLastNode;
-				for (int i = mIndex; i > position; i--)
+				ptrNode = getLastNode();
+				for (int i = index; i > position; i--)
 				{
 					ptrNode = ptrNode->getPrev();
 				}
@@ -158,7 +463,7 @@ public:
 			}
 			else
 			{
-				ptrNode = mFirstNode;
+				ptrNode = getFirstNode();
 				for (int i = 0; i < position; i++)
 				{
 					ptrNode = ptrNode->getNext();
@@ -168,17 +473,17 @@ public:
 				ptrNode->setPrev(newNode);
 			}
 		}
-		else if (position == 0 && mIndex > 0)
+		else if (position == 0 && index > 0)
 		{
-			Node<T>* newNode = new Node<T>(value, mFirstNode, nullptr);
-			mFirstNode->setPrev(newNode);
-			mFirstNode = newNode;
+			Node<T>* newNode = new Node<T>(value, getFirstNode(), nullptr);
+			getFirstNode()->setPrev(newNode);
+			setFirstNode(newNode);
 		}
-		else if (mIndex == 0)
+		else if (index == 0)
 		{
 			Add(value);
 		}
-		else if (position == mIndex)
+		else if (position == index)
 		{
 			Add(value);
 		}
@@ -189,21 +494,23 @@ public:
 		}
 		if (!isError)
 		{
-			mIndex++;
+			setIndex(getIndex() + 1);
 		}
-		
+
 	}
+
 
 	// Overwrites the value of a node at the given position.
 	void ReplaceAt(T value, uint32_t position)
 	{
-		Node<T>* ptrNode;
-		if (position > 0 && position < mIndex - 1)
+		uint32_t index = getIndex();
+		Node<T>* ptrNode = nullptr;
+		if (position > 0 && position > index - 1)
 		{
-			if (position > mIndex / 2)
+			if (position > index / 2)
 			{
 				ptrNode = mLastNode;
-				for (int i = mIndex; i > position; i--)
+				for (int i = index; i > position; i--)
 				{
 					ptrNode = ptrNode->getPrev();
 				}
@@ -219,15 +526,15 @@ public:
 				ptrNode->setValue(value);
 			}
 		}
-		else if (position == 0 && mIndex > 0)
+		else if (position == 0 && index > 0)
 		{
 			mFirstNode->setValue(value);
 		}
-		else if (mIndex == 0)
+		else if (index == 0)
 		{
-			Add(value);
+			std::cerr << "Error: Could not replace. The list seems to be empty!\n";
 		}
-		else if (position == mIndex)
+		else if (position == index)
 		{
 			mLastNode->setValue(value);
 		}
@@ -240,17 +547,18 @@ public:
 	// Returns the length of the linked list as an integer
 	int getLength()
 	{
-		return mIndex;
+		return getIndex();
 	}
 
 	// Deletes the node at the given position
-	void deleteAt(int position)
+	void deleteAt(uint32_t position)
 	{
-		Node<T>* ptrNode;
+		uint32_t index = getIndex();
+		Node<T>* ptrNode = nullptr;
 		if (position > mIndex / 2)
 		{
 			ptrNode = mLastNode;
-			for (int i = mIndex; i > position; i--)
+			for (uint32_t i = mIndex; i > position; i--)
 			{
 				ptrNode = ptrNode->getPrev();
 			}
@@ -258,8 +566,8 @@ public:
 		}
 		else
 		{
-			ptrNode = mFirstNode;
-			for (int i = 0; i < position; i++)
+			ptrNode = getFirstNode();
+			for (uint32_t i = 0; i < position; i++)
 			{
 				ptrNode = ptrNode->getNext();
 			}
@@ -267,38 +575,40 @@ public:
 		}
 
 	}
+
 	// Delete an amount of nodes
 	// amount is the number of nodes to be deleted
 	// fromStart deceides if it deletes the nodes from the beginning or the end of the linked list
-	void deleteAmount(int amount, bool fromStart)
+	void deleteAmount(uint32_t amount, bool fromStart)
 	{
-		if (amount < mIndex)
+		uint32_t index = getIndex();
+		if (amount < index)
 		{
 			if (fromStart)
 			{
 				for (int i = 0; i < amount; i++)
 				{
-					mFirstNode = mFirstNode->getNext();
+					setFirstNode(getFirstNode()->getNext());
 				}
-				mFirstNode->getPrev()->setNext(nullptr);
-				mFirstNode->setPrev(nullptr);
+				getFirstNode()->getPrev()->setNext(nullptr);
+				getFirstNode()->setPrev(nullptr);
 			}
 			else
 			{
 				for (int i = 0; i < amount; i++)
 				{
-					mLastNode = mLastNode->getPrev();
+					setLastNode(getLastNode()->getPrev());
 				}
-				mLastNode->getNext()->setPrev(nullptr);
-				mLastNode->setNext(nullptr);
+				getLastNode()->getNext()->setPrev(nullptr);
+				getLastNode()->setNext(nullptr);
 			}
-			mIndex -= amount;
+			index -= amount;
 		}
 		else
 		{
-			std::cerr << "Error: Could not delete " << amount << " nodes. The length of the linked list is " << mIndex << ". Do you want to delete all? y/n";
+			std::cerr << "Error: Could not delete " << amount << " nodes. The length of the linked list is " << mIndex << ". \nDo you want to delete all? y/n\n";
 			std::string answer = "";
-			while (answer != "y" || answer != "n" || answer != "Y" || answer != "N")
+			while ((answer != "y") && (answer != "n") && (answer != "Y") && (answer != "N"))
 			{
 				std::cin >> answer;
 				if (answer == "y" || answer == "Y")
@@ -307,15 +617,16 @@ public:
 				}
 				else if (answer == "n" || answer == "N")
 				{
-					std::cerr << "Canceled function 'deleteAmount'.";
+					std::cerr << "Canceled function 'deleteAmount'.\n";
 				}
 				else
 				{
-					std::cerr << "Wrong input. Please input 'y' or 'n'.";
+					std::cerr << "Wrong input. Please input 'y' or 'n'.\n";
 				}
 			}
-			
+
 		}
+		setIndex(index);
 	}
 	// Deletes all nodes
 	void deleteAll()
@@ -329,11 +640,11 @@ public:
 	void deleteValue(T value)
 	{
 		Node<T>* ptrNode = mFirstNode;
-		int found = 0;
-		int index = 0;
-		int length = mIndex;
+		uint32_t found = 0;
+		uint32_t index = 0;
+		uint32_t length = getIndex();
 
-		while(ptrNode != nullptr)
+		while (ptrNode != nullptr)
 		{
 			if (ptrNode->getValue() == value)
 			{
@@ -358,19 +669,39 @@ public:
 		}
 	}
 
+	// Returns true if the list is empty
+	bool isEmpty()
+	{
+		if (mFirstNode == nullptr)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-	// ToDo: 
+	// Inverts the list.
+	void invertList()
+	{
+		Node<T>* ptrNode = mLastNode;
+		mLastNode = mFirstNode;
+		mFirstNode = ptrNode;
 
-	// - Zugriff auf Element nach Position
-	// - Zugriff auf Element nach Inhalt
-	// - Überprüfen ob Liste leer ist
-	// - Umkehren der Liste
-	// - Sortieren der Liste
-	// - Iteration über die Liste um Operationen auf Element auszuführen
-	// - Zusammenführen von Listen
+		while (ptrNode != nullptr)
+		{
+			Node<T>* tempNode = ptrNode;
+			ptrNode = ptrNode->getPrev();
+			tempNode->setPrev(tempNode->getNext());
+			tempNode->setNext(ptrNode);
+		}
+
+	}
 
 
-
+	// Make privat when done testing 
+	public:
 
 	void Print()
 	{
@@ -429,7 +760,7 @@ public:
 
 	void PrintReverseWithIndex()
 	{
-		uint32_t index = mIndex -1;
+		uint32_t index = mIndex - 1;
 		if (mFirstNode == nullptr)
 		{
 			std::cerr << "Error: No entries!\n";
@@ -445,6 +776,7 @@ public:
 			std::cout << "\n";
 		}
 	}
+
 };
 
 #endif
